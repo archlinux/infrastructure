024ad0f9b9bb40f802b198c51daf1bce83b38f87
Sven-Hendrik Haase <svenstaro@gmail.com>
2016-06-13 20:59:05 +0200

    dbscripts: Add special user sudoer.d file

---
+++ roles/dbscripts/files/sudoers.d
@@ -0,0 +1,8 @@
+%dev ALL=(svn-packages) NOPASSWD:/usr/bin/svnserve
+%dev ALL=(svn-packages) NOPASSWD:/usr/bin/svn
+#%dev ALL=(svn-community) NOPASSWD:/usr/bin/svnserve
+%tu ALL=(svn-community) NOPASSWD:/usr/bin/svnserve
+%tu ALL=(svn-community) NOPASSWD:/usr/bin/svn
+
+sourceballs ALL=(svn-community) NOPASSWD:/usr/bin/svn
+sourceballs ALL=(svn-packages) NOPASSWD:/usr/bin/svn
