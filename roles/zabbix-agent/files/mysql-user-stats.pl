3bd07309bf01977fd68b581c0dbdeb4b5f505e24
Florian Pritz <bluewind@xinu.at>
2018-02-16 15:11:12 +0100

    zabbix/mysql-userstats: Move user stats to subkey for future expansion
    We might want to monitor client/table/index stats in the future too
    without having to change the zabbix item configs.
    
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- roles/zabbix-agent/files/mysql-user-stats.pl
+++ roles/zabbix-agent/files/mysql-user-stats.pl
@@ -9,4 +9,4 @@ my $config = $ARGV[0] // "$ENV{HOME}/.my.cnf";
 
 my $db = DBI->connect("DBI:mysql:mysql_read_default_file=$config");
 $db->{FetchHashKeyName} = "NAME_lc";
-print encode_json($db->selectall_hashref("select * from information_schema.user_statistics", "user"));
+print encode_json({user => $db->selectall_hashref("select * from information_schema.user_statistics", "user")});
