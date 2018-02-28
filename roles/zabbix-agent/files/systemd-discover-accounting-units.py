2de25873454322252c9d1eadc496a87da1dbcb0f
Florian Pritz <bluewind@xinu.at>
2018-02-28 18:48:04 +0100

    zabbix/accounting: Discover .slice units
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- roles/zabbix-agent/files/systemd-discover-accounting-units.py
+++ roles/zabbix-agent/files/systemd-discover-accounting-units.py
@@ -11,14 +11,22 @@ units = systemd1_manager.ListUnits()
 
 discovered = []
 
-def get_unit_prop(interface, propname):
+def get_unit_prop_interface(obj, interface, propname):
     try:
-        return unit_properties.Get('org.freedesktop.systemd1.Service', propname)
+        return obj.Get(interface, propname)
     except dbus.exceptions.DBusException as err:
         if err.get_dbus_name() == 'org.freedesktop.DBus.Error.UnknownProperty':
             return None
         else:
             raise
+
+def get_unit_prop(obj, propname):
+    interfaces = ['org.freedesktop.systemd1.Service', 'org.freedesktop.systemd1.Slice']
+    for interface in interfaces:
+        ret = get_unit_prop_interface(obj, interface, propname)
+        if ret:
+            return ret
+    return None
 
 for u in units:
     unit_properties = dbus.Interface(bus.get_object('org.freedesktop.systemd1', u[6]), dbus_interface='org.freedesktop.DBus.Properties')
