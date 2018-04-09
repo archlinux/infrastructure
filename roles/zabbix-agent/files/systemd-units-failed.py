12e05ae1f77ddbec7ba0814bd28f33d509408218
Florian Pritz <bluewind@xinu.at>
2018-04-09 09:34:06 +0200

    zabbix: Ignore failed syncrepo services
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- roles/zabbix-agent/files/systemd-units-failed.py
+++ roles/zabbix-agent/files/systemd-units-failed.py
@@ -2,10 +2,13 @@
 
 import dbus
 
+
+ignore = set(['syncrepo.service', 'syncrepo_arch32.service'])
+
 bus = dbus.SystemBus()
 systemd1 = bus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
 systemd1_manager = dbus.Interface(systemd1, dbus_interface='org.freedesktop.systemd1.Manager')
 
 units = systemd1_manager.ListUnits()
-for unit in filter(lambda u: u[3] == 'failed', units):
+for unit in filter(lambda u: u[3] == 'failed' and u[0] not in ignore, units):
     print(unit[0])
