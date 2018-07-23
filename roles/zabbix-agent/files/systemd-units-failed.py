96fad709d852446807ee557b869a67c9d6d461af
Bartłomiej Piotrowski <bpiotrowski@archlinux.org>
2018-07-23 22:59:49 +0200

    zabbix-agent: format systemd-units-failed.py with black

--- roles/zabbix-agent/files/systemd-units-failed.py
+++ roles/zabbix-agent/files/systemd-units-failed.py
@@ -3,12 +3,14 @@
 import dbus
 
 
-ignore = set(['syncrepo.service', 'syncrepo_arch32.service'])
+ignore = set(["syncrepo.service", "syncrepo_arch32.service"])
 
 bus = dbus.SystemBus()
-systemd1 = bus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
-systemd1_manager = dbus.Interface(systemd1, dbus_interface='org.freedesktop.systemd1.Manager')
+systemd1 = bus.get_object("org.freedesktop.systemd1", "/org/freedesktop/systemd1")
+systemd1_manager = dbus.Interface(
+    systemd1, dbus_interface="org.freedesktop.systemd1.Manager"
+)
 
 units = systemd1_manager.ListUnits()
-for unit in filter(lambda u: u[3] == 'failed' and u[0] not in ignore, units):
+for unit in filter(lambda u: u[3] == "failed" and u[0] not in ignore, units):
     print(unit[0])
