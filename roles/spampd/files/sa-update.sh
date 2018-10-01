c08cd648ed4704263995d7305f396c39ad6f2c7f
Florian Pritz <bluewind@xinu.at>
2018-10-01 10:06:20 +0200

    sa-update.sh: Exit with error when sa-update fails
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- roles/spampd/files/sa-update.sh
+++ roles/spampd/files/sa-update.sh
@@ -2,6 +2,14 @@
 
 set -e
 
-/usr/bin/vendor_perl/sa-update --channelfile /etc/mail/spamassassin/update-channels --gpgkeyfile /etc/mail/spamassassin/update-gpgkeys || exit 0
+if ! /usr/bin/vendor_perl/sa-update --channelfile /etc/mail/spamassassin/update-channels --gpgkeyfile /etc/mail/spamassassin/update-gpgkeys; then
+	exitcode=$?
+	if ((exitcode == 1)); then
+		exit 0
+	else
+		echo "sa-update failed"
+		exit 1
+	fi
+fi
 /usr/bin/vendor_perl/sa-compile --quiet
 systemctl restart spampd
