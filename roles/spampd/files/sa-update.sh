fdaf0a1bb579786b23d4d128380260b9c93345cf
Florian Pritz <bluewind@xinu.at>
2018-10-01 10:12:56 +0200

    sa-update.sh: Fix error handling not getting exit code due to if
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- roles/spampd/files/sa-update.sh
+++ roles/spampd/files/sa-update.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if ! /usr/bin/vendor_perl/sa-update --channelfile /etc/mail/spamassassin/update-channels --gpgkeyfile /etc/mail/spamassassin/update-gpgkeys; then
+/usr/bin/vendor_perl/sa-update --channelfile /etc/mail/spamassassin/update-channels --gpgkeyfile /etc/mail/spamassassin/update-gpgkeys || {
 	exitcode=$?
 	if ((exitcode == 1)); then
 		exit 0
@@ -10,6 +10,6 @@ if ! /usr/bin/vendor_perl/sa-update --channelfile /etc/mail/spamassassin/update-
 		echo "sa-update failed"
 		exit 1
 	fi
-fi
+}
 /usr/bin/vendor_perl/sa-compile --quiet
 systemctl restart spampd
