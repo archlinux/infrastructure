08e411bfb25ffcb068f47d78dd37fd92ddbb060c
Florian Pritz <bluewind@xinu.at>
2019-01-10 16:04:14 +0100

    dbscripts: Add sudoers entries for package archive
    Context: https://lists.archlinux.org/pipermail/arch-projects/2019-January/005063.html
    
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- roles/dbscripts/files/sudoers.d
+++ roles/dbscripts/files/sudoers.d
@@ -4,5 +4,8 @@
 %tu ALL=(svn-community) NOPASSWD:/usr/bin/svnserve
 %tu ALL=(svn-community) NOPASSWD:/usr/bin/svn
 
+%dev ALL = (archive) NOPASSWD: /packages/db-archive
+%tu  ALL = (archive) NOPASSWD: /community/db-archive
+
 sourceballs ALL=(svn-community) NOPASSWD:/usr/bin/svn
 sourceballs ALL=(svn-packages) NOPASSWD:/usr/bin/svn
