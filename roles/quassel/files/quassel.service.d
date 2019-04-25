678b40ca0b9aa4c8d03a5d877a19beb4868953d8
Jelle van der Waa <jelle@vdwaa.nl>
2019-04-25 21:33:41 +0200

    Remove unrequired oinetd sevice

--- roles/quassel/files/quassel.service.d
+++ roles/quassel/files/quassel.service.d
@@ -1,3 +1,3 @@
 [Service]
 ExecStart=
-ExecStart=/usr/bin/quasselcore --configdir=/var/lib/quassel --ident-daemon --ident-port=1114 --strict-ident --syslog --require-ssl
+ExecStart=/usr/bin/quasselcore --configdir=/var/lib/quassel --syslog --require-ssl
