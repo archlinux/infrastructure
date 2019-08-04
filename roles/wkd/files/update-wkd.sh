420d0717be0cc1acd71c6c3293e6e56e64220124
Florian Pritz <bluewind@xinu.at>
2019-08-04 11:05:25 +0200

    orion: Add gpg WKD directory for pacman
    Maintain a web key directory with our gpg keys. The update script is run
    automatically whenever the archlinux-keyring package on orion is
    upgraded.
    
    https://bugs.archlinux.org/task/63171
    
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

---
+++ roles/wkd/files/update-wkd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+workdir="$1"
+
+if [[ -z "$workdir" ]]; then
+	echo "Error: workdir not set" >&2
+	exit 1
+fi
+
+export GNUPGHOME=/etc/pacman.d/gnupg
+
+mkdir -p "$workdir/openpgpkey/archlinux.org/hu"
+
+for email in $(gpg --list-options show-only-fpr-mbox --list-keys | grep '@archlinux.org' | cut -d' ' -f2); do
+	wkd_hash="$(/usr/lib/gnupg/gpg-wks-client --print-wkd-hash "$email" | cut -d' ' -f1)"
+	outfile="$workdir/openpgpkey/archlinux.org/hu/$wkd_hash"
+	gpg --export "$email" > "$outfile"
+
+	# TODO: return error if filesize of $outfile is >= 64kB; https://dev.gnupg.org/T4607#127792
+done
