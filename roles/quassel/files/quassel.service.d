015f338b2b44b6e7f0bbf4278d853b1df692782d
Jelle van der Waa <jelle@vdwaa.nl>
2019-09-04 21:03:25 +0200

    harden quassel service

--- roles/quassel/files/quassel.service.d
+++ roles/quassel/files/quassel.service.d
@@ -2,3 +2,9 @@
 ExecStart=
 ExecStart=/usr/bin/quasselcore --configdir=/var/lib/quassel --ident-daemon --ident-listen=::,0.0.0.0 --ident-port=113 --strict-ident --syslog --require-ssl
 AmbientCapabilities=CAP_NET_BIND_SERVICE
+PrivateTmp=yes
+NoNewPrivileges=yes
+ProtectSystem=full
+ProtectControlGroups=yes
+ProtectKernelModules=yes
+ProtectKernelTunables=yes
