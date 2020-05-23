989c55f9d1533a37a6244dd1ae96723a2044e4e4
Sven-Hendrik Haase <svenstaro@gmail.com>
2020-05-23 17:09:44 +0200

    kcadm_wrapper.sh: Reformat and always use -r archlinux for convenience

--- misc/kcadm_wrapper.sh
+++ misc/kcadm_wrapper.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
 # Script that wraps kcadm.sh to securely get our credentials.
+# It always operates on the realm `archlinux` for convenience.
 #
 # Example invocation:
 # misc/kcadm_wrapper.sh get realms
@@ -10,4 +11,10 @@
 # See for general kcadm usage:
 # https://github.com/keycloak/keycloak-documentation/blob/master/server_admin/topics/admin-cli.adoc
 
-kcadm "$@" --no-config --server https://accounts.archlinux.org/auth --realm master --user $(misc/get_key.py group_vars/all/vault_keycloak.yml vault_keycloak_admin_user) --password $(misc/get_key.py group_vars/all/vault_keycloak.yml vault_keycloak_admin_password)
+kcadm "$@" \
+    -r archlinux \
+    --no-config \
+    --server https://accounts.archlinux.org/auth \
+    --realm master \
+    --user $(misc/get_key.py group_vars/all/vault_keycloak.yml vault_keycloak_admin_user) \
+    --password $(misc/get_key.py group_vars/all/vault_keycloak.yml vault_keycloak_admin_password)
