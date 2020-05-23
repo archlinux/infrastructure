7e3ab62a235293cbe1090d4623065ce56942154b
Sven-Hendrik Haase <svenstaro@gmail.com>
2020-05-23 17:05:37 +0200

    kcadm_wrapper.sh: Yet more docs

--- misc/kcadm_wrapper.sh
+++ misc/kcadm_wrapper.sh
@@ -7,5 +7,7 @@
 # misc/kcadm_wrapper.sh get authentication/flows
 # Refer to the API docs for which resources exist:
 # https://www.keycloak.org/docs-api/10.0/rest-api/index.html
-#
+# See for general kcadm usage:
+# https://github.com/keycloak/keycloak-documentation/blob/master/server_admin/topics/admin-cli.adoc
+
 kcadm "$@" --no-config --server https://accounts.archlinux.org/auth --realm master --user $(misc/get_key.py group_vars/all/vault_keycloak.yml vault_keycloak_admin_user) --password $(misc/get_key.py group_vars/all/vault_keycloak.yml vault_keycloak_admin_password)
