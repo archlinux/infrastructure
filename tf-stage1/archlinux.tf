7f90b9edfb86ce261e2891712adff383ec6fdbba
Kristian Klausen <kristian@klausen.dk>
2020-12-26 03:40:41 +0100

    tf-stage1/archlinux: Avoid similar mx/SPF records by using for_each

--- tf-stage1/archlinux.tf
+++ tf-stage1/archlinux.tf
@@ -280,14 +280,6 @@ resource "hetznerdns_record" "archlinux_org_origin_caa" {
   type    = "CAA"
 }
 
-resource "hetznerdns_record" "archlinux_org_origin_mx" {
-  zone_id = hetznerdns_zone.archlinux.id
-  name    = "@"
-  ttl     = 600
-  value   = "10 mail"
-  type    = "MX"
-}
-
 resource "hetznerdns_record" "archlinux_org_origin_ns3" {
   zone_id = hetznerdns_zone.archlinux.id
   name    = "@"
@@ -376,22 +368,6 @@ resource "hetznerdns_record" "archlinux_org_aur_aaaa" {
   name    = "aur"
   value   = hcloud_server.aur.ipv6_address
   type    = "AAAA"
-}
-
-resource "hetznerdns_record" "archlinux_org_aur_mx" {
-  zone_id = hetznerdns_zone.archlinux.id
-  name    = "aur"
-  ttl     = 600
-  value   = "10 mail"
-  type    = "MX"
-}
-
-resource "hetznerdns_record" "archlinux_org_aur_txt" {
-  zone_id = hetznerdns_zone.archlinux.id
-  name    = "aur"
-  ttl     = 600
-  value   = "\"v=spf1 a ?all\""
-  type    = "TXT"
 }
 
 resource "hetznerdns_record" "archlinux_org_aur_dev_a" {
@@ -600,14 +576,6 @@ resource "hetznerdns_record" "archlinux_org_master_key_aaaa" {
   type    = "AAAA"
 }
 
-resource "hetznerdns_record" "archlinux_org_master_key_mx" {
-  zone_id = hetznerdns_zone.archlinux.id
-  name    = "master-key"
-  ttl     = 600
-  value   = "10 mail"
-  type    = "MX"
-}
-
 resource "hetznerdns_record" "archlinux_org_matrix_a" {
   zone_id = hetznerdns_zone.archlinux.id
   name    = "matrix"
@@ -645,21 +613,25 @@ resource "hetznerdns_record" "archlinux_org_mail_aaaa" {
   type    = "AAAA"
 }
 
-resource "hetznerdns_record" "archlinux_org_origin_txt" {
+resource "hetznerdns_record" "archlinux_org_origin_mx" {
+  for_each = toset(["@", "aur", "master-key"])
+
   zone_id = hetznerdns_zone.archlinux.id
-  name    = "@"
+  name    = each.value
+  ttl     = 600
+  value   = "10 mail"
+  type    = "MX"
+}
+
+resource "hetznerdns_record" "archlinux_org_origin_txt" {
+  for_each = toset(["@", "aur", "mail", "master-key"])
+
+  zone_id = hetznerdns_zone.archlinux.id
+  name    = each.value
   ttl     = 600
   # mail.archlinux.org
   value = "\"v=spf1 ip4:95.216.189.61 ip6:2a01:4f9:c010:3052::1 ~all\""
   type  = "TXT"
-}
-
-resource "hetznerdns_record" "archlinux_org_mail_txt" {
-  zone_id = hetznerdns_zone.archlinux.id
-  name    = "mail"
-  ttl     = 600
-  value   = "\"v=spf1 include:archlinux.org -all\""
-  type    = "TXT"
 }
 
 resource "hetznerdns_record" "archlinux_org_domainkey_dkim-ed25519_txt" {
