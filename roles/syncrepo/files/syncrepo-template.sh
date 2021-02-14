73bf287a81ae6918a11e3944f6d40120c62b6ff4
Jelle van der Waa <jelle@archlinux.org>
2021-02-14 17:43:20 +0100

    Lock before deleting files
    As reported in FS#69614 the lock should be before the delete for when
    another sync job is running rsync can't move the files as they are
    removed.

--- roles/syncrepo/files/syncrepo-template.sh
+++ roles/syncrepo/files/syncrepo-template.sh
@@ -59,11 +59,11 @@ lastupdate_url=''
 [ ! -d "${target}" ] && mkdir -p "${target}"
 [ ! -d "${tmp}" ] && mkdir -p "${tmp}"
 
-# Cleanup any temporary files from old run that might remain.
-find "${target}" -name '.~tmp~' -exec rm -r {} +
-
 exec 9>"${lock}"
 flock -n 9 || exit
+
+# Cleanup any temporary files from old run that might remain.
+find "${target}" -name '.~tmp~' -exec rm -r {} +
 
 rsync_cmd() {
 	local -a cmd=(rsync -rtlH --safe-links --delete-after ${VERBOSE} "--timeout=600" "--contimeout=60" -p \
