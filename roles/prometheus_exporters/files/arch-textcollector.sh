4222e7f9bb6c63d6a9516106a79ce25e4c3ff536
Leonidas Spyropoulos <artafinde@gmail.com>
2021-05-15 11:00:44 +0100

    prometheus: Add monitoring and alert for orphan and foreign packages
    Closes: #332
    
    Signed-off-by: Leonidas Spyropoulos <artafinde@gmail.com>

--- roles/prometheus_exporters/files/arch-textcollector.sh
+++ roles/prometheus_exporters/files/arch-textcollector.sh
@@ -18,6 +18,8 @@ trap "rm -f $TMP_FILE" EXIT
 
 updates=$(/usr/bin/checkupdates | wc -l)
 secupdates=$(/usr/bin/arch-audit -u | wc -l)
+orphans=$(/usr/bin/pacman -Qdtq | wc -l)
+foreigns=$(/usr/bin/pacman -Qmq | wc -l)
 
 echo "# HELP pacman_updates_pending number of pending updates from pacman" >> $TMP_FILE
 echo "# TYPE pacman_updates_pending gauge" >> $TMP_FILE
@@ -26,5 +28,13 @@ echo "pacman_updates_pending $updates" >> $TMP_FILE
 echo "# HELP pacman_security_updates_pending number of pending updates from pacman" >> $TMP_FILE
 echo "# TYPE pacman_security_updates_pending gauge" >> $TMP_FILE
 echo "pacman_security_updates_pending $secupdates" >> $TMP_FILE
+
+echo "# HELP pacman_orphans number of orphan packages from pacman" >> $TMP_FILE
+echo "# TYPE pacman_orphans gauge" >> $TMP_FILE
+echo "pacman_orphans $orphans" >> $TMP_FILE
+
+echo "# HELP pacman_foreigns number of foreign packages from pacman" >> $TMP_FILE
+echo "# TYPE pacman_foreigns gauge" >> $TMP_FILE
+echo "pacman_foreigns $foreigns" >> $TMP_FILE
 
 mv -f $TMP_FILE $PROM_FILE
