4dc0dd4c5648a594bf67302e642464773db799b2
Thorben Günther <admin@xenrox.net>
2021-07-09 15:53:46 +0200

    prometheus_exporters: Improve arch-textcollector
    Add number of pacnew/pacsave files and print non explicit installed
    optdepends as orphans as well.

--- roles/prometheus_exporters/files/arch-textcollector.sh
+++ roles/prometheus_exporters/files/arch-textcollector.sh
@@ -18,8 +18,9 @@ trap "rm -f $TMP_FILE" EXIT
 
 updates=$(/usr/bin/checkupdates | wc -l)
 secupdates=$(/usr/bin/arch-audit -u | wc -l)
-orphans=$(/usr/bin/pacman -Qdtq | wc -l)
+orphans=$(/usr/bin/pacman -Qdttq | wc -l)
 foreigns=$(/usr/bin/pacman -Qmq | wc -l)
+pacnew=$(/usr/bin/pacdiff -o | wc -l)
 
 echo "# HELP pacman_updates_pending number of pending updates from pacman" >> $TMP_FILE
 echo "# TYPE pacman_updates_pending gauge" >> $TMP_FILE
@@ -36,5 +37,9 @@ echo "pacman_orphans $orphans" >> $TMP_FILE
 echo "# HELP pacman_foreigns number of foreign packages from pacman" >> $TMP_FILE
 echo "# TYPE pacman_foreigns gauge" >> $TMP_FILE
 echo "pacman_foreigns $foreigns" >> $TMP_FILE
+
+echo "# HELP pacman_pacnew number of pacnew/pacsave files" >> $TMP_FILE
+echo "# TYPE pacman_pacnew gauge" >> $TMP_FILE
+echo "pacman_pacnew $pacnew" >> $TMP_FILE
 
 mv -f $TMP_FILE $PROM_FILE
