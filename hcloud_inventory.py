452090f60b8ff9c4efb59d16fd3ba6048bc10320
Evangelos Foutras <evangelos@foutrelis.com>
2021-07-12 15:44:35 +0300

    hcloud_inventory: Optimize --list to avoid --host calls
    By adding a top-level element called "_meta" to the --list response,
    Ansible will not call the inventory script with --host for each host
    thus saving a lot of time and many requests to the Hetzner Cloud API.
    
    The speed-up is significant; `ansible-inventory --list` is down from
    about 1 minute to just 7 seconds in my testing (with ~60ms latency).

--- hcloud_inventory.py
+++ hcloud_inventory.py
@@ -19,12 +19,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def list_running_hosts(client):
-    return [server.name for server in client.servers.get_all()]
-
-
-def get_host_details(client, host):
-    server = client.servers.get_by_name(host)
+def get_host_details(server):
     return {'ansible_host': server.public_net.ipv4.ip,
             'ansible_port': 22,
             'ansible_user': "root"}
@@ -34,13 +29,14 @@ def main():
     args = parse_args()
     loaded = load_vault('misc/vault_hetzner.yml')
     client = Client(token=loaded["hetzner_cloud_api_key"])
+    servers = client.servers.get_all()
 
+    hostvars = {server.name: get_host_details(server) for server in servers}
     if args.list:
-        hosts = list_running_hosts(client=client)
-        json.dump({'hcloud': hosts}, sys.stdout)
+        hosts = [server.name for server in servers]
+        json.dump({'hcloud': hosts, '_meta': {'hostvars': hostvars}}, sys.stdout)
     else:
-        details = get_host_details(client, args.host)
-        json.dump(details, sys.stdout)
+        json.dump(hostvars[args.host], sys.stdout)
 
 
 if __name__ == '__main__':
