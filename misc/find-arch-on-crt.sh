1234dcec73517055dd8abe0f006c2ed1032a3021
Evangelos Foutras <evangelos@foutrelis.com>
2022-06-17 22:59:33 +0300

    prometheus: split crt.sh lookups into new script
    roles/prometheus/defaults/main.yml used to include a comment with the
    commands used to generate a list of HTTPS endpoints to check. Move it
    into a proper script and fix it to generate the correct current list.

---
+++ misc/find-arch-on-crt.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# crt.sh lookup script to generate part of the blackbox_targets.http_prometheus
+# list (stored in roles/prometheus/defaults/main.yml) based on SSL certificates
+
+set -eo pipefail
+
+readonly DOMAINS=(
+  archlinux.org
+  pkgbuild.com
+)
+readonly LOOKUP_URLS=(
+  "${DOMAINS[@]/#/https://crt.sh/?exclude=expired&deduplicate=Y&output=json&q=}"
+)
+
+names() {
+  curl -sf "${LOOKUP_URLS[@]}" | jq -r '.[].name_value' | sort -u
+}
+
+prometheus_targets() {
+  names \
+  | sed 's|^|https://|' \
+  | sed '/monitoring\|dashboards/ s|$|/healthz|' \
+  | sed '/mta-sts/ s|$|/.well-known/mta-sts.txt|' \
+  | sed '/openpgpkey/ s|openpgpkey\.\(.*\)|&/.well-known/openpgpkey/\1/policy|' \
+  | sed '/repos\.arch/ s|$|/lastupdate|' \
+  | sed '/static\.conf/ s|$|/README.md|' \
+  | sed -r '/geo\.mirror|(bugs-old|coc|git|status)\.arch/d' \
+  | xargs -P8 -I{} sh -c 'curl -m 10 -sf -o /dev/null {} && echo "    "- {}' \
+  | sort
+}
+
+if [[ $1 = targets ]]; then
+  prometheus_targets
+elif [[ -n $1 ]]; then
+  echo >&2 'error: the first argument can only be empty or "targets"'
+  exit 1
+else
+  names
+fi
