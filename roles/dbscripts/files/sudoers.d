4159a61f5e7b14553f3cd508622974fe68bae46d
Jelle van der Waa <jelle@archlinux.org>
2023-05-18 16:47:03 +0200

    dbscripts: switch to Git packaging
    This drops all svn specific functionality and switches to dbscripts git
    version. Drops the community repository as it's merged into extra.

--- roles/dbscripts/files/sudoers.d
+++ roles/dbscripts/files/sudoers.d
@@ -1,11 +1,7 @@
-%dev ALL=(svn-packages) NOPASSWD:/usr/bin/svnserve
-%dev ALL=(svn-packages) NOPASSWD:/usr/bin/svn
-#%dev ALL=(svn-community) NOPASSWD:/usr/bin/svnserve
-%tu ALL=(svn-community) NOPASSWD:/usr/bin/svnserve
-%tu ALL=(svn-community) NOPASSWD:/usr/bin/svn
-
 %dev ALL = (archive) NOPASSWD: /packages/db-archive
-%tu  ALL = (archive) NOPASSWD: /community/db-archive
+%junior-dev ALL = (archive) NOPASSWD: /packages/db-archive
+%packager ALL = (archive) NOPASSWD: /packages/db-archive
+%junior-packager ALL = (archive) NOPASSWD: /packages/db-archive
 
 sourceballs ALL=(svn-community) NOPASSWD:/usr/bin/svn
 sourceballs ALL=(svn-packages) NOPASSWD:/usr/bin/svn
