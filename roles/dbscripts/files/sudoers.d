c8a32ef165f7887a6fbf629ddf886ff497816f3f
Jelle van der Waa <jelle@archlinux.org>
2023-07-09 17:14:45 +0200

    drop svn-community/svn-packages

--- roles/dbscripts/files/sudoers.d
+++ roles/dbscripts/files/sudoers.d
@@ -2,6 +2,3 @@
 %junior-dev ALL = (archive) NOPASSWD: /packages/db-archive
 %packager ALL = (archive) NOPASSWD: /packages/db-archive
 %junior-packager ALL = (archive) NOPASSWD: /packages/db-archive
-
-sourceballs ALL=(svn-community) NOPASSWD:/usr/bin/svn
-sourceballs ALL=(svn-packages) NOPASSWD:/usr/bin/svn
