02960645de2eff27026c28be3e3372f0f4bbfa53
Kristian Klausen <kristian@klausen.dk>
2023-10-15 22:38:23 +0200

    Rename tu-bylaws.aur.a.o to package-maintainer-bylaws.aur.a.o
    The role has been renamed[1], so rename the bylaw subdomain.
    
    tu-bylaws.aur.a.o will be kept around for some time redirecting to
    package-maintainer-bylaws.aur.a.o.
    
    [1] https://gitlab.archlinux.org/archlinux/rfcs/-/merge_requests/7
    
    Ref #533

--- tf-stage1/archlinux.tf
+++ tf-stage1/archlinux.tf
@@ -163,15 +163,15 @@ locals {
   #   - value equals the pages verification code
   #
   archlinux_org_gitlab_pages = {
-    "conf"                  = "60a06a1c02e42b36c3b4919f4d6de6bf"
-    "whatcanidofor"         = "d9e45851002a623e10f6954ff9a85d21"
-    "openpgpkey"            = "d20c137368e26dcc3db56d45a368e729"
-    "openpgpkey.master-key" = "3eea8f39a9b473a5dc7c188366f84072"
-    "bugs-old"              = "1f3308c8d5763eecb4f9013291aeeac4"
-    "tu-bylaws.aur"         = "bbafd3ed82f336e0c52d3eb9774b2432"
-    "reproducible-notes"    = "8c657f2f2720db1c3db63be89605cf0d"
-    "terms"                 = "0b62a71af2aa85fb491295b543b4c3d2"
-    "patchwork"             = "37eeadf24d5cd6614e8edb1f12868a5e"
+    "conf"                          = "60a06a1c02e42b36c3b4919f4d6de6bf"
+    "whatcanidofor"                 = "d9e45851002a623e10f6954ff9a85d21"
+    "openpgpkey"                    = "d20c137368e26dcc3db56d45a368e729"
+    "openpgpkey.master-key"         = "3eea8f39a9b473a5dc7c188366f84072"
+    "bugs-old"                      = "1f3308c8d5763eecb4f9013291aeeac4"
+    "package-maintainer-bylaws.aur" = "680c89d189c8f342cc00bcb624d813a3"
+    "reproducible-notes"            = "8c657f2f2720db1c3db63be89605cf0d"
+    "terms"                         = "0b62a71af2aa85fb491295b543b4c3d2"
+    "patchwork"                     = "37eeadf24d5cd6614e8edb1f12868a5e"
   }
 
   archlinux_page_gitlab_pages = {
@@ -290,22 +290,23 @@ locals {
   # Example:
   # dev                      = { value = "www", ttl = 3600 }
   archlinux_org_cname = {
-    archive       = { value = "gemini" }
-    dev           = { value = "www" }
-    ipxe          = { value = "www" }
-    mailman       = { value = "redirect" }
-    packages      = { value = "www" }
-    ping          = { value = "redirect" }
-    planet        = { value = "www" }
-    registry      = { value = "gitlab" }
-    repos         = { value = "gemini" }
-    rsync         = { value = "gemini" }
-    sources       = { value = "gemini" }
-    "static.conf" = { value = "redirect" }
-    status        = { value = "redirect" }
-    svn           = { value = "gemini" }
-    coc           = { value = "redirect" }
-    git           = { value = "redirect" }
+    archive         = { value = "gemini" }
+    dev             = { value = "www" }
+    ipxe            = { value = "www" }
+    mailman         = { value = "redirect" }
+    packages        = { value = "www" }
+    ping            = { value = "redirect" }
+    planet          = { value = "www" }
+    registry        = { value = "gitlab" }
+    repos           = { value = "gemini" }
+    rsync           = { value = "gemini" }
+    sources         = { value = "gemini" }
+    "static.conf"   = { value = "redirect" }
+    status          = { value = "redirect" }
+    svn             = { value = "gemini" }
+    coc             = { value = "redirect" }
+    git             = { value = "redirect" }
+    "tu-bylaws.aur" = { value = "redirect" }
 
     # MTA-STS
     mta-sts               = { value = "mail" }
