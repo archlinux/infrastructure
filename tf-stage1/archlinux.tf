d25078410c14174efa0f2da44e347c38763a01b3
Kristian Klausen <kristian@klausen.dk>
2025-09-02 22:01:48 +0200

    Draft: ACC sponsored geomirror/archive
    https://gitlab.archlinux.org/archlinux/infrastructure/-/merge_requests/1012

--- tf-stage1/archlinux.tf
+++ tf-stage1/archlinux.tf
@@ -445,6 +445,16 @@ locals {
     repro4 = {
       ipv6_address = "2001:1470:fffd:3050::189:1"
     }
+    "umea.mirror" = {
+      ipv4_address = "194.71.11.121"
+      ipv6_address = "2001:6b0:19:2::121"
+      http3        = true
+    }
+    "umea.archive" = {
+      ipv4_address = "194.71.11.121"
+      ipv6_address = "2001:6b0:19:2::121"
+      http3        = true
+    }
     www = {
       ipv4_address = hcloud_server.machine["homedir.archlinux.org"].ipv4_address
       ipv6_address = hcloud_server.machine["homedir.archlinux.org"].ipv6_address
