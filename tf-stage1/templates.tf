3601870825984c98c2a2434a691225010eeb4086
Christian Heusel <christian@heusel.eu>
2025-10-30 13:38:49 +0100

    tf-stage1: Add tf logic to reserve floating ips
    This is useful for the upcoming bastion host and for reserving IPs to be
    used with vendors such as HAproxy.
    
    Signed-off-by: Christian Heusel <christian@heusel.eu>

--- tf-stage1/templates.tf
+++ tf-stage1/templates.tf
@@ -312,3 +312,25 @@ resource "hcloud_zone_rrset" "geo_ns" {
     { value = "london.mirror.pkgbuild.com." }
   ]
 }
+
+resource "hcloud_floating_ip" "machine_floating_ipv4_reserve" {
+  for_each = {
+    for name, machine in local.machines : name => machine if can(machine.floating_ipv4)
+  }
+
+  name              = "ipv4-${each.key}-reserve"
+  type              = "ipv4"
+  server_id         = hcloud_server.machine[each.key].id
+  delete_protection = true
+}
+
+resource "hcloud_floating_ip" "machine_floating_ipv6_reserve" {
+  for_each = {
+    for name, machine in local.machines : name => machine if can(machine.floating_ipv6)
+  }
+
+  name              = "ipv6-${each.key}-reserve"
+  type              = "ipv6"
+  server_id         = hcloud_server.machine[each.key].id
+  delete_protection = true
+}
