10da50a93c2d90cfd1ce08b2b41f218f3bb0e7d7
Christian Heusel <christian@heusel.eu>
2025-12-27 17:55:48 +0100

    one-shots: Drop the one-shots directory
    Signed-off-by: Christian Heusel <christian@heusel.eu>

--- one-shots/keycloak-keyfetcher/get_fingerprint.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -s https://accounts.archlinux.org/realms/archlinux/protocol/saml/descriptor  | xmllint --xpath '//*[local-name()="X509Certificate"]/text()' - | base64 -d | sha1sum | cut -d ' ' -f1 | sed -e 's/.\{2\}/&:/g' | sed 's/:$//' | tr '[:lower:]' '[:upper:]'
