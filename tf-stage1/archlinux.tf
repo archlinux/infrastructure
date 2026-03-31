a16b5dd6be4f9f2b1b1a3cbb7cdbe3885b21be48
Leonidas Spyropoulos <artafinde@archlinux.org>
2026-03-31 20:40:12 +0100

    feat(codesearch): implement zoekt-based code search role
    Link: https://gitlab.archlinux.org/archlinux/infrastructure/-/merge_requests/1168

--- tf-stage1/archlinux.tf
+++ tf-stage1/archlinux.tf
@@ -135,6 +135,10 @@ locals {
       server_type = "cx23"
       domain      = "bumpbuddy"
     }
+    "codesearch.archlinux.org" = {
+      server_type = "cx33"
+      domain      = "codesearch"
+    }
     "dashboards.archlinux.org" = {
       server_type = "cx23"
       domain      = "dashboards"
@@ -865,6 +869,13 @@ resource "hcloud_volume" "monitoring" {
   name              = "monitoring"
   size              = 215
   server_id         = hcloud_server.machine["monitoring.archlinux.org"].id
+  delete_protection = true
+}
+
+resource "hcloud_volume" "codesearch" {
+  name              = "codesearch"
+  size              = 800
+  server_id         = hcloud_server.machine["codesearch.archlinux.org"].id
   delete_protection = true
 }
 
